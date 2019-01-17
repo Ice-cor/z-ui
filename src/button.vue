5eababffc06152d66c979a87166ce7edc1f75b2f
zwq <zhong0102@qq.com>
2019-01-17 17:23:45 +0800

    icon位置及loading状态

--- src/button.vue
+++ src/button.vue
@@ -1,16 +1,58 @@
 <template>
-    <button class="z-button">
+  <button
+    class="z-button"
+    :disabled="loading"
+  >
+    <svg
+      :class="['icon','loading']"
+      aria-hidden="true"
+      v-if="loading"
+    >
+      <use :xlink:href="`#icon-loading`"></use>
+    </svg>
+    <svg
+      :class="['icon',position]"
+      aria-hidden="true"
+      v-if="name"
+    >
+      <use :xlink:href="`#icon-${name}`"></use>
+    </svg>
+    <div class="content">
       <slot>按钮</slot>
-    </button>
+    </div>
+
+  </button>
 </template>
 
 <script>
 export default {
-  name: "g-button",
-};
+  props: {
+    name: {},
+    position: {
+      type: String,
+      default: 'left',
+      validator: function(value) {
+        // 这个值必须匹配下列字符串中的一个
+        return ['left', 'right'].indexOf(value) !== -1
+      }
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+      validator: function(value) {
+        // 这个值必须匹配下列字符串中的一个
+        return [true, false].indexOf(value) !== -1
+      }
+    }
+  },
+  name: 'g-button',
+  data() {
+    return {}
+  }
+}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .z-button {
   font-size: var(--font-size);
   height: var(--button-height);
@@ -18,6 +60,9 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: flex;
+  justify-content: center;
+  align-items: center;
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -26,6 +71,32 @@ export default {
   }
   &:focus {
     outline: none;
+  }
+  &:disabled{
+    background-color: #999;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.2em;
+  }
+  > .content {
+    order: 2;
+  }
+  > .icon.right {
+    order: 3;
+    margin-left: 0.2em;
+    margin-right: 0;
+  }
+  .loading {
+    animation: loading 1s infinite linear;
+  }
+  @keyframes loading {
+    0% {
+      transform: rotate(0);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
   }
 }
 </style>
